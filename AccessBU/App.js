8038caf667b6dbd5bed65ccc38a3efc8abc61d87
Andres Garcia <andresfg@bu.edu>
2023-11-18 14:47:42 -0500

    Added map to home screen

--- AccessBU/App.js
+++ AccessBU/App.js
@@ -5,6 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import * as Location from 'expo-location';
 import { StatusBar } from 'expo-status-bar';
+import MapView, { Marker } from 'react-native-maps';
 
 function HomeScreen() {
   const [location, setLocation] = useState(null);
@@ -24,8 +25,25 @@ function HomeScreen() {
 
   return (
     <View style={styles.container}>
-      <Text>Home Screen</Text>
-      {location && <Text>Latitude: {location.coords.latitude}, Longitude: {location.coords.longitude}</Text>}
+      {location && (
+        <MapView
+          style={styles.map}
+          initialRegion={{
+            latitude: location.coords.latitude,
+            longitude: location.coords.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        >
+          <Marker
+            coordinate={{
+              latitude: location.coords.latitude,
+              longitude: location.coords.longitude,
+            }}
+            title="My Location"
+          />
+        </MapView>
+      )}
       <StatusBar style="auto" />
     </View>
   );
@@ -46,28 +64,28 @@ export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
-            let iconName;
+  screenOptions={({ route }) => ({
+    tabBarIcon: ({ focused, color, size }) => {
+      let iconName;
 
-            if (route.name === 'Home') {
-              iconName = focused ? 'home' : 'home-outline';
-            } else if (route.name === 'About') {
-              iconName = focused ? 'information-circle' : 'information-circle-outline';
-            }
+      if (route.name === 'Home') {
+        iconName = focused ? 'home' : 'home-outline';
+      } else if (route.name === 'About') {
+        iconName = focused ? 'information-circle' : 'information-circle-outline';
+      }
 
-            // You can return any component that you like here!
-            return <Ionicons name={iconName} size={size} color={color} />;
-          },
-        })}
-        tabBarOptions={{
-          activeTintColor: 'tomato',
-          inactiveTintColor: 'gray',
-        }}
-      >
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="About" component={AboutScreen} />
-      </Tab.Navigator>
+      // You can return any component that you like here!
+      return <Ionicons name={iconName} size={size} color={color} />;
+    },
+  })}
+  tabBarOptions={{
+    activeTintColor: 'tomato',
+    inactiveTintColor: 'gray',
+  }}
+>
+  <Tab.Screen name="Home" component={HomeScreen} />
+  <Tab.Screen name="About" component={AboutScreen} />
+</Tab.Navigator>
     </NavigationContainer>
   );
 }
@@ -78,5 +96,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
   },
 });
