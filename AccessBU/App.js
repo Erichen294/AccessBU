c5cdc25c1091bb00e224284b39686457dc0a1692
Andres Garcia <andresfg@bu.edu>
2023-11-18 16:07:31 -0500

    Added field for user to enter destination with google places autocomplete

--- AccessBU/App.js
+++ AccessBU/App.js
@@ -1,18 +1,24 @@
 import React, { useEffect, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import * as Location from 'expo-location';
 import { StatusBar } from 'expo-status-bar';
-import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
+import MapView, { Marker, Polyline, PROVIDER_GOOGLE } from 'react-native-maps';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 
 const GOOGLE_MAPS_APIKEY = 'AIzaSyCxKzb1TTNef3e0wcQcnurbtLHSZendI3Y'; // Replace with your Google Maps API key
 
 function HomeScreen() {
   const [location, setLocation] = useState(null);
+  const [destination, setDestination] = useState(null);
+  const [directions, setDirections] = useState(null);
+
 
   useEffect(() => {
+    let locationSubscription;
+
     (async () => {
       let { status } = await Location.requestForegroundPermissionsAsync();
       if (status !== 'granted') {
@@ -20,24 +26,62 @@ function HomeScreen() {
         return;
       }
 
-      let location = await Location.getCurrentPositionAsync({});
-      setLocation(location);
+      locationSubscription = await Location.watchPositionAsync(
+        {
+          accuracy: Location.Accuracy.High,
+          timeInterval: 1000,
+        },
+        (newLocation) => {
+          setLocation(newLocation);
+        },
+      );
     })();
+
+    return () => {
+      if (locationSubscription) {
+        locationSubscription.remove();
+      }
+    };
   }, []);
+
+  const getDirections = async () => {
+    try {
+      const response = await fetch(
+        `https://maps.googleapis.com/maps/api/directions/json?origin=${location.coords.latitude},${location.coords.longitude}&destination=${destination}&mode=walking&key=${GOOGLE_MAPS_APIKEY}`
+      );
+      const data = await response.json();
+      const points = data.routes[0].overview_polyline.points;
+      setDirections(points);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <View style={styles.container}>
+      <View style={styles.directions}>
+        <GooglePlacesAutocomplete
+          placeholder='Enter Destination'
+          onPress={(data, details = null) => {
+            setDestination(details.formatted_address);
+          }}
+          query={{
+            key: GOOGLE_MAPS_APIKEY,
+            language: 'en',
+          }}
+        />
+        <Button title="Get Directions" onPress={getDirections} />
+      </View>
       {location && (
         <MapView
           provider={PROVIDER_GOOGLE}
           style={styles.map}
-          initialRegion={{
+          region={{
             latitude: location.coords.latitude,
             longitude: location.coords.longitude,
             latitudeDelta: 0.0922,
             longitudeDelta: 0.0421,
           }}
-          customMapStyle={GOOGLE_MAPS_APIKEY}
         >
           <Marker
             coordinate={{
@@ -46,12 +90,15 @@ function HomeScreen() {
             }}
             title="My Location"
           />
+          {directions && <Polyline coordinates={directions} />}
         </MapView>
       )}
       <StatusBar style="auto" />
     </View>
   );
 }
+
+// ... rest of your code
 
 
 function AboutScreen() {
@@ -101,6 +148,15 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  directions: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    zIndex: 1,
+    backgroundColor: '#fff',
+    padding: 10,
   },
   map: {
     ...StyleSheet.absoluteFillObject,
