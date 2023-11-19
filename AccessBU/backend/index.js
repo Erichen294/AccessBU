78d2685302c7766ff0ebe9ff15eb2a59f326622d
howeyour <jli3469@bu.edu>
2023-11-18 23:40:00 -0500

    Update index.js

--- AccessBU/backend/index.js
+++ AccessBU/backend/index.js
@@ -1,25 +1,35 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const uri = "mongodb+srv://accessbu:smartsys@cluster0.twbrrdm.mongodb.net/?retryWrites=true&w=majority";
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
+async function retrieveData() {
+    const client = new MongoClient(uri, {
+        serverApi: {
+            version: ServerApiVersion.v1,
+            strict: true,
+            deprecationErrors: true,
+        }
+        });
 
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
+    try {
+        await client.connect();
+
+        const database = client.db('accessbu'); // Replace 'your_database_name' with your actual database name
+        const collection = database.collection('nodes'); // Replace 'your_collection_name' with your actual collection name
+
+        // Find all documents in the collection
+        const documents = await collection.find({}).toArray();
+        console.log('Retrieved documents:');
+        // console.log(documents[1].Latitude);
+        // console.log(documents[2].Latitude);
+        // console.log(documents[3].Latitude);
+        // console.log(documents[4].Latitude);
+        for (let i = 0; i < documents.length; i++) {
+            console.log(documents[i].Latitude);
+        }
+    } finally {
+        await client.close();
+    }
 }
-run().catch(console.dir);
+
+retrieveData().catch(console.error);
